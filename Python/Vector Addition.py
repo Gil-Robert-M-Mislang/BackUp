f1d66ef71bb426ac3d06b3a50c7764ce2fb7d8fe
Gil Robert M. Mislang <gilmislangschoolpup@gmail.com>
2025-09-18 23:15:06 +0800

    New Features

--- Python/Vector Addition.py
+++ Python/Vector Addition.py
@@ -1,13 +1,17 @@
 import numpy as np
 from fractions import Fraction
 import math
+import pandas #for data cleaning
 
 x = np.array([1,2,3])
 y = np.array([1, 0 ,0 ])
 z = np.array([2,-6,9,0])
+A = np.array([[1,2],[1,-1]])
 
 dot = x - y
 
 distance = np.linalg.norm(dot)
 
-print(distance)
+cube_np = np.linalg.matrix_power(A,12)
+
+print(cube_np)
