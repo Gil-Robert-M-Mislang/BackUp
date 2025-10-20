aa3eaa523c7cd8165e050951eac68e53118cbe55
Gil Robert M. Mislang <gilmislangschoolpup@gmail.com>
2025-10-20 20:40:33 +0800

    Adding comments

---
+++ Python/RREF.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#input matrix
+    #--> create a table for the input for great UI
+    #--> Input elements function
+    #--> Base the size of the table on inputs in rows and columns
+
+def main():
+    row = int(input("Enter the number of rows: "))
+    column = int(input("Enter the number of columns: "))
+
+    print("Enter the entries: ")
+
+    
