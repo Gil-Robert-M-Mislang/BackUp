19f6da453017e859625e4dea7ce5851eabacdd9a
Gil Robert M. Mislang <gilmislangschoolpup@gmail.com>
2025-12-30 09:37:01 +0800

    Finished Group 2 source code

--- Python/Group2.py
+++ Python/Group2.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sympy as sp
 
 def LI_tester(input):
     size = input.shape
@@ -18,27 +19,48 @@ def LI_tester(input):
         
         return True
     
-def Sum_Projection(input, size):
+def Sum_Projection(input, size, index):
+    result_matrix = np.zeros(size[1])
+
+    for i in range(index):
+        norm = np.linalg.norm(input[i])
+        result_matrix += ((input[index] @ input[i])/ norm**2) * input[i] 
+
+    return result_matrix
+
+def Normalization(input, size):
     result_matrix = np.empty(size)
 
+    for i in range(size[0]):
+        result_matrix[i] = input[i]/np.linalg.norm(input[i])
+    
     return result_matrix
 
 def Gram_Schmidt(input, size):
     result_matrix = np.empty(size)
 
     for i in range(size[0]):
-        for j in range(size[0]):
-            result_matrix[i]
-    #For loop in every row excluding first row
-    #Create another function that sums the gram schmidt
+        if i == 0:
+            result_matrix[i] = input[i]
+        else:
+            result_matrix[i] = input[i] - Sum_Projection(result_matrix, size, i)
+
+    result_matrix = Normalization(result_matrix, size)
+
+    return result_matrix
 
 def main():
-    matrix = np.array([[1,1,-1,1],[-1,0,1,1],[0,-1,-1,2]])
+    matrix = np.array([[1,2,-1,0],[2,2,0,1],[1,1,1,0]])
     matrix_size = matrix.shape
 
     independence = LI_tester(matrix)
     if independence == True:
-        Gram_Schmidt(matrix, matrix_size)
+        result_matrix = Gram_Schmidt(matrix, matrix_size)
+        result_matrix[np.abs(result_matrix) < 1e-12] = 0
+        end_matrix = sp.Matrix(result_matrix).applyfunc(sp.nsimplify)
+        print(f"The orthonormal matrix of the given matrix is: ")
+        for row in end_matrix.tolist():
+            print(row)
     else:
         print("Input Error: The input is not linearly independent")
 
